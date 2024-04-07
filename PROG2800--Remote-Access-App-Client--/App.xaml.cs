05dbcfa7c82e0b442156e5aebd43ced7d2c81a02
Thomas <Thomas@DESKTOP-29HUHGP>
2024-04-07 10:16:16 -0700

    Add project files.

---
+++ PROG2800--Remote-Access-App-Client--/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PROG280__Remote_Access_App_Client__
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
