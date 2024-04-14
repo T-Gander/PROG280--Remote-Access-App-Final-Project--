452b9ce118de7f84c1a3d6b2f4ed37a4c34f6b1d
Thomas Gander - School <thomas.gander@mymhc.ca>
2024-04-13 18:44:58 -0600

    N/A

--- PROG2800--Remote-Access-App-Client--/RemoteWindow.xaml.cs
+++ PROG2800--Remote-Access-App-Client--/RemoteWindow.xaml.cs
@@ -4,6 +4,7 @@ using PROG280__Remote_Access_App_Data__;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -24,15 +25,35 @@ namespace PROG2800__Remote_Access_App_Client__
     /// <summary>
     /// Interaction logic for RemoteWindow.xaml
     /// </summary>
-    public partial class RemoteWindow : Window
+    public partial class RemoteWindow : INotifyPropertyChanged
     {
-        //public static BitmapImage? Frame { get; set; }
+        public BitmapImage? Frame 
+        { 
+            get 
+            { 
+                return _frame; 
+            }
+            set
+            {
+                _frame = value;
+                OnPropertyChanged(nameof(Frame));
+            }
+        }
+        private BitmapImage? _frame;
 
         public RemoteWindow()
         {
             InitializeComponent();
-            DataContext = NetworkConnected.CurrentFrame;
+            Frame = NetworkConnected.CurrentFrame;
+            DataContext = this;
             //Open a messaging window.
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
